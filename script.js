2f4b16164ceaa711331a3b74985f64a111124d2f
Alee <124084660+allisonurquhart@users.noreply.github.com>
2025-04-17 21:24:24 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+<a class="carousel-control-next" href="#carouselSpecials" data-slide="prev" onclick="$('#carouselSpecials').carousel('prev')">‹</a>
+<a class="carousel-control-prev" href="#carouselSpecials" data-slide="next" onclick="$('#carouselSpecials').carousel('next')">›</a>
